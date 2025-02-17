69d903c27847910d488ecddf170de2bba6833f04
yuriko192 <ivanwijaya19@yahoo.co.id>
2025-02-17 13:56:50 +0700

    fix external code execution

--- app/main.c
+++ app/main.c
@@ -15,140 +15,119 @@ const size_t TYPE_COMMAND_LEN = sizeof(TYPE_COMMAND) - 1;
 const char *BUILTIN_COMMAND_LIST[] = {
     EXIT_COMMAND,
     ECHO_COMMAND,
-    TYPE_COMMAND};
+    TYPE_COMMAND
+};
 
 const size_t BUILTIN_COMMAND_COUNT = sizeof(BUILTIN_COMMAND_LIST) / sizeof(BUILTIN_COMMAND_LIST[0]);
 
-int is_valid_builtin_command(char *inp_cmd)
-{
-  for (size_t i = 0; i < BUILTIN_COMMAND_COUNT; i++)
-  {
-    if (strcmp(inp_cmd, BUILTIN_COMMAND_LIST[i]) == 0)
-      return 1;
-  }
-  return 0;
-}
-
-char *get_executable_fullpath(char *inp_cmd)
-{
-  char *path_dirs = strdup(getenv("PATH"));
-  static char full_path[1024];
-
-  char *dir = strtok(path_dirs, ":");
-  while (dir != NULL)
-  {
-    snprintf(full_path, sizeof(full_path), "%s/%s", dir, inp_cmd);
-
-    if (access(full_path, X_OK) == 0)
-    { // is_executable
-      free(path_dirs);
-      return full_path;
+int is_valid_builtin_command(char *inp_cmd) {
+    for (size_t i = 0; i < BUILTIN_COMMAND_COUNT; i++) {
+        if (strcmp(inp_cmd, BUILTIN_COMMAND_LIST[i]) == 0)
+            return 1;
     }
-
-    dir = strtok(NULL, ":");
-  }
-
-  free(path_dirs);
-  return NULL;
-}
-
-void execute_type_command(char *inp_cmd)
-{
-  if (is_valid_builtin_command(inp_cmd))
-  {
-    printf("%s is a shell builtin\n", inp_cmd);
-    return;
-  }
-
-  char *execPath = get_executable_fullpath(inp_cmd);
-  if (execPath != NULL)
-  {
-    printf("%s is %s\n", inp_cmd, execPath);
-    return;
-  }
-
-  printf("%s: not found\n", inp_cmd);
-}
-
-int execute_external_process(char *input)
-{
-  char *ext_exec_name = strtok(input, " ");
-  char *execPath = get_executable_fullpath(ext_exec_name);
-  if (execPath == NULL)
-  {
     return 0;
-  }
-
-  char *argv[10];
-  int argc = 0;
-  char *token = strtok(input, " ");
-  while (token != NULL && argc < 10)
-  {
-    argv[argc++] = token;
-    token = strtok(NULL, " ");
-  }
-
-  int pid = fork();
-  if (pid == -1)
-  {
-    perror("fork failed");
-    return 1;
-  }
-
-  if (pid == 0)
-  {
-    execv(execPath, argv);
-    perror("execv");
-    exit(1);
-  }
-  else
-  {
-    int status;
-    waitpid(pid, &status, 0);
-  }
 }
 
-int main()
-{
-  // Flush after every printf
-  setbuf(stdout, NULL);
+char *get_executable_fullpath(char *inp_cmd) {
+    char *path_dirs = strdup(getenv("PATH"));
+    static char full_path[1024];
 
-  // Wait for user input
-  char input[100];
+    char *dir = strtok(path_dirs, ":");
+    while (dir != NULL) {
+        snprintf(full_path, sizeof(full_path), "%s/%s", dir, inp_cmd);
 
-  while (1)
-  {
-    printf("$ ");
-    fgets(input, 100, stdin);
+        if (access(full_path, X_OK) == 0) {
+            // is_executable
+            free(path_dirs);
+            return full_path;
+        }
 
-    input[strlen(input) - 1] = '\0';
-
-    if (strncmp(input, EXIT_COMMAND, EXIT_COMMAND_LEN) == 0)
-    {
-      return 0;
+        dir = strtok(NULL, ":");
     }
 
-    if (strncmp(input, ECHO_COMMAND, ECHO_COMMAND_LEN) == 0)
-    {
-      printf("%s\n", input + ECHO_COMMAND_LEN + 1);
-      continue;
+    free(path_dirs);
+    return NULL;
+}
+
+void execute_type_command(char *inp_cmd) {
+    if (is_valid_builtin_command(inp_cmd)) {
+        printf("%s is a shell builtin\n", inp_cmd);
+        return;
     }
 
-    if (strncmp(input, TYPE_COMMAND, TYPE_COMMAND_LEN) == 0)
-    {
-      execute_type_command(input + TYPE_COMMAND_LEN + 1);
-      continue;
+    char *execPath = get_executable_fullpath(inp_cmd);
+    if (execPath != NULL) {
+        printf("%s is %s\n", inp_cmd, execPath);
+        return;
     }
 
-    int pid = execute_external_process(input);
-    if (pid != 0)
-    {
-      printf("Program Signature: %d\n", pid);
-      continue;
+    printf("%s: not found\n", inp_cmd);
+}
+
+int execute_external_process(char *input) {
+    char *argv[10];
+    int argc = 0;
+    char *token = strtok(input, " ");
+    while (token != NULL && argc < 10) {
+        argv[argc++] = token;
+        token = strtok(NULL, " ");
+    }
+    argv[argc] = NULL;
+
+    char *execPath = get_executable_fullpath(argv[0]);
+    if (execPath == NULL) {
+        return 0;
     }
 
-    printf("%s: command not found\n", input);
-  }
+    int pid = fork();
+    if (pid == -1) {
+        perror("fork failed");
+        return 1;
+    }
 
-  return 0;
+    if (pid == 0) {
+        execv(execPath, argv);
+        perror("execv");
+        exit(1);
+    } else {
+        int status;
+        waitpid(pid, &status, 0);
+    }
+}
+
+int main() {
+    setbuf(stdout, NULL);
+
+    char input[100];
+
+    while (1) {
+        printf("$ ");
+        fgets(input, 100, stdin);
+
+        input[strlen(input) - 1] = '\0';
+
+        if (strncmp(input, EXIT_COMMAND, EXIT_COMMAND_LEN) == 0) {
+            return 0;
+        }
+
+        if (strncmp(input, ECHO_COMMAND, ECHO_COMMAND_LEN) == 0) {
+            printf("%s\n", input + ECHO_COMMAND_LEN + 1);
+            continue;
+        }
+
+        if (strncmp(input, TYPE_COMMAND, TYPE_COMMAND_LEN) == 0) {
+            execute_type_command(input + TYPE_COMMAND_LEN + 1);
+            continue;
+        }
+
+        int pid = execute_external_process(input);
+        if (pid != 0) {
+            printf("Program Signature: %d\n", pid);
+            continue;
+        }
+
+        printf("%s: command not found\n", input);
+    }
+
+    return 0;
 }
