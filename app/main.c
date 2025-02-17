695635c6a8ae52d7fa30f60a5616b5863a161c0e
yuriko192 <ivanwijaya19@yahoo.co.id>
2025-02-17 09:55:24 +0700

    init REPL

--- app/main.c
+++ app/main.c
@@ -6,15 +6,17 @@ int main() {
   // Flush after every printf
   setbuf(stdout, NULL);
 
-  // Uncomment this block to pass the first stage
-  printf("$ ");
-
   // Wait for user input
   char input[100];
-  fgets(input, 100, stdin);
 
-  input[strlen(input) - 1] = '\0';
-  printf("%s: command not found\n", input);
+  while(true){
+    printf("$ ");
+    fgets(input, 100, stdin);
+
+    input[strlen(input) - 1] = '\0';
+    printf("%s: command not found\n", input);
+  }
+  
 
   return 0;
 }
