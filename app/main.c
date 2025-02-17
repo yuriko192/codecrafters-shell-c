597b21d42d877e06a60f297738dafa918349cee2
yuriko192 <ivanwijaya19@yahoo.co.id>
2025-02-17 10:43:38 +0700

    refactor type command

--- app/main.c
+++ app/main.c
@@ -2,11 +2,41 @@
 #include <string.h>
 
 const char EXIT_COMMAND[] = "exit";
-const int EXIT_COMMAND_LEN = strlen(EXIT_COMMAND);
 const char ECHO_COMMAND[] = "echo";
-const int ECHO_COMMAND_LEN = strlen(ECHO_COMMAND);
 const char TYPE_COMMAND[] = "type";
+
+const int EXIT_COMMAND_LEN = strlen(EXIT_COMMAND);
+const int ECHO_COMMAND_LEN = strlen(ECHO_COMMAND);
 const int TYPE_COMMAND_LEN = strlen(TYPE_COMMAND);
+
+const char *BUILTIN_COMMAND_LIST[] = {
+  EXIT_COMMAND,
+  ECHO_COMMAND,
+  TYPE_COMMAND
+};
+
+const size_t BUILTIN_COMMAND_COUNT = sizeof(BUILTIN_COMMAND_LIST) / sizeof(BUILTIN_COMMAND_LIST[0]);
+
+
+int is_valid_builtin_command(char *inp_cmd)
+{
+  for (size_t i = 0; i < BUILTIN_COMMAND_COUNT; i++)
+  {
+    if (strcmp(inp_cmd, BUILTIN_COMMAND_LIST[i]) == 0) return 1;
+  }
+  return 0;
+}
+
+void execute_type_command(char *inp_cmd)
+{
+  if (is_valid_builtin_command(inp_cmd))
+  {
+    printf("%s is a shell builtin\n", inp_cmd);
+    return;
+  }
+
+  printf("%s: not found\n", inp_cmd);
+}
 
 int main()
 {
@@ -36,14 +66,7 @@ int main()
 
     if (strncmp(input, TYPE_COMMAND, TYPE_COMMAND_LEN) == 0)
     {
-      char *inp_cmd = input + TYPE_COMMAND_LEN + 1;
-      if (strcmp(inp_cmd, ECHO_COMMAND) == 0 ||
-         strcmp(inp_cmd, EXIT_COMMAND) == 0 ||
-         strcmp(inp_cmd, TYPE_COMMAND) == 0){
-        printf("%s is a shell builtin\n", inp_cmd);
-      }else{
-        printf("%s: not found\n", inp_cmd);
-      }
+      execute_type_command(input + TYPE_COMMAND_LEN + 1);
       continue;
     }
 
