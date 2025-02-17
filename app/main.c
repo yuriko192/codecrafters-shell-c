3dd4db04a0ffbc1570add0b5e988dc7be4bbc867
yuriko192 <ivanwijaya19@yahoo.co.id>
2025-02-17 10:22:50 +0700

    add type

--- app/main.c
+++ app/main.c
@@ -1,35 +1,51 @@
 #include <stdio.h>
 #include <string.h>
 
-const char EXIT_COMMAND[] = "exit 0";
+const char EXIT_COMMAND[] = "exit";
+const int EXIT_COMMAND_LEN = strlen(EXIT_COMMAND);
+const char ECHO_COMMAND[] = "echo";
+const int ECHO_COMMAND_LEN = strlen(ECHO_COMMAND);
+const char TYPE_COMMAND[] = "type";
+const int TYPE_COMMAND_LEN = strlen(TYPE_COMMAND);
 
-int main() {
+int main()
+{
   // Flush after every printf
   setbuf(stdout, NULL);
 
   // Wait for user input
   char input[100];
 
-  while(1){
+  while (1)
+  {
     printf("$ ");
     fgets(input, 100, stdin);
 
     input[strlen(input) - 1] = '\0';
 
-    if (strcmp(input, EXIT_COMMAND)==0)
+    if (strncmp(input, EXIT_COMMAND, EXIT_COMMAND_LEN) == 0)
     {
       return 0;
     }
 
-    if (strncmp(input, "echo", strlen("echo")) == 0) {
-      printf("%s\n", input + 5);
+    if (strncmp(input, ECHO_COMMAND, ECHO_COMMAND_LEN) == 0)
+    {
+      printf("%s\n", input + ECHO_COMMAND_LEN + 1);
       continue;
     }
-    
+
+    if (strncmp(input, TYPE_COMMAND, TYPE_COMMAND_LEN) == 0)
+    {
+      char *inp_cmd = input + TYPE_COMMAND_LEN + 1;
+      if (strncmp(inp_cmd, ECHO_COMMAND) == 0 ||
+         strncmp(inp_cmd, EXIT_COMMAND) == 0){
+        printf("%s is a shell builtin\n", inp_cmd);
+        continue;
+      }
+    }
 
     printf("%s: command not found\n", input);
   }
-  
 
   return 0;
 }
